7e8142d080014e6d1004b25038ed14c22fec829d
Damilola <damilola.adekoya@payjoy.com>
2024-06-30 20:00:13 -0400

    project-setup

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Welcome to Home Page</div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'HomePage',
+  });
+  </script>
+  
+  <style scoped>
+  /* Your styles here */
+  </style>
+  
